c1034546b0f27a7090697f458b08ba6aa30bcb7e
cmy <1018617499@qq.com>
2018-07-07 16:12:38 +0800

    IdeaTest

---
+++ src/main/java/com/cmy/App.java
@@ -0,0 +1,14 @@
+package com.cmy;
+import static java.lang.System.out;
+/**
+ * 演示静态导包
+ *
+ */
+public class App 
+{
+    //省略System 可直接使用该类的静态成员
+    public static void main( String[] args )
+    {
+        out.println( "Hello World!" );
+    }
+}
